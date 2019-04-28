52099ab88c783dd9e767732be3c277030312c62f
Matt W <matt@matts-mbp.wireless.davidson.edu>
2019-04-28 19:02:24 -0400

    dropdown menu functioning

--- homepage.php
+++ homepage.php
@@ -23,7 +23,30 @@
 </div>
  
  <a href="https://espn.com"> Here is a cool button selection </a>
+
+<br>
+<br>
+<br>
+ <select id="teams-menu" onchange="dropdownSelect()">
+  <option value="all" selected>All teams</option>
+  <option value="tournament">Tournament Teams</option>
+  <option value="non-tournament">Non-tournament teams</option>
+  <option value="Davidson">Davidson</option>
+</select>
 </body>
+
+
+<!-- DROPDOWN MENU https://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript -->
+<script>
+function dropdownSelect() {
+  var e = document.getElementById("teams-menu");
+  var text = e.options[e.selectedIndex].text;
+  alert(text); // do anything once selected (probably run a query)
+}
+
+</script>
+
+
 
 <script> 
 /* When the user clicks on the button,
@@ -46,4 +69,20 @@ window.onclick = function(event) {
   }
 } 
 </script>
+
+
+<!-- <script> 
+var pointsX = [300], pointsY = [450];
+for(var i = 0; i < pointsX.length; i++){
+    var div = document.createElement('div');
+    div.className = 'dot';
+    div.style.left = pointsX[i] + 'px';
+    div.style.top = pointsY[i] + 'px';
+    document.getElementById('wrapper').appendChild(div);
+} 
+</script> -->
+
+<span class="dot"></span>
+
+
 </html>
