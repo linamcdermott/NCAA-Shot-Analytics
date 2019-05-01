65e6ddc6b0a6fd018fbe32a20f38d6f49cf60cce
Lina McDermott <limcdermott@davidson.edu>
2019-05-01 13:51:14 -0400

    starting changes to dataImporter

--- DataImporter.py
+++ DataImporter.py
@@ -8,31 +8,27 @@ def importData():
 
     db = client.baskets
 
-    player = db["player"]
     team = db["team"]
     game = db["game"]
-    shot = db["shot"]
-    roster = db["roster"]
-    '''
-    x = player.delete_many({})
+
     x = team.delete_many({})
     x = game.delete_many({})
-    x = shot.delete_many({})
-    x = roster.delete_many({})
 
-    player.drop_indexes()
     team.drop_indexes()
     game.drop_indexes()
-    shot.drop_indexes()
-    roster.drop_indexes()
 
-
-    player.create_index([('name', pymongo.ASCENDING)], unique=True)
     team.create_index([('school', pymongo.ASCENDING)], unique=True)
     game.create_index([('date', pymongo.ASCENDING), ("home", pymongo.ASCENDING ), ('away', pymongo.ASCENDING)], unique=True)
-    #shot.create_index([('date', pymongo.ASCENDING)], unique=True)
-    roster.create_index([('player_id', pymongo.ASCENDING), ("season", pymongo.ASCENDING )], unique=True)
 
+    drafted_list = []
+
+    filename = "drafted.txt"
+    in_file = open(filename, 'r')
+    for line in in_file:
+        line = line.strip('\n')
+        drafted_list.append(line)
+
+    in_file.close()
 
     for i in range(13,19):
 
@@ -44,106 +40,22 @@ def importData():
             csv_reader.next()
 
             for row in csv_reader:
-
                 if any (row):
+
                     school_1 = row[1]
                     if(row[1][-3:] == " ST"):
                         school_1 = row[1] + "ATE"
-                    # school_1 = row[1].replace(" ST", " STATE")
-                  
-
+                    
                     school_2 = row[2]
                     if(row[2][-2:] == "ST"):
                         school_2 = row[2] + "ATE"
-                    
-                    
-                    #add team first
-                    team_doc_1 = {
-                        "school" : school_1
-                    }
 
-                    team_doc_2 = {
-                        "school" : school_2
-                    }
-
-                    try:
-                        team_id = team.insert_one(team_doc_1).inserted_id
-                    except:
-                        team_id = team.find_one({"school" : school_1}).get('_id')
-                    try:
-                        opponent_id = team.insert_one(team_doc_2).inserted_id
-                    except:
-                        opponent_id = team.find_one({"school" : school_2}).get('_id')
-
-                    #insert player
-                    player_doc = {
-                        "name" : row[5],
-                        "drafted": False
-                    }
-                    try:
-                        player_id = player.insert_one(player_doc).inserted_id
-                    except:
-                        player_id = player.find_one({"name" : row[5]}).get('_id')
-
-                    #assisting player
-                    if(row[9] != "n/a"):
-                        assist_doc = {
-                            "name" : row[9],
-                            "drafted": False
-                        }
-                        try:
-                            assist_id = player.insert_one(assist_doc).inserted_id
-                        except:
-                            assist_id = player.find_one({"name" : row[9]}).get('_id')
-
-                        roster_doc_assist = {
-                            "season" : "20" + str(i) + "-" + "20" + str(i+1),
-                            "player_id": assist_id,
-                            "team_id": team_id,
-                            "tournament": False
-                        }
-                        try:
-                            roster_id_assist = roster.insert_one(roster_doc_assist).inserted_id
-                        except:
-                            pass
-                    else:
-                        assist_id = None
-
-                    #insert roster
-                    roster_doc = {
-                        "season" : "20" + str(i) + "-" + "20" + str(i+1),
-                        "player_id": player_id,
-                        "team_id": team_id,
-                        "tournament": False
-                    }
-                    try:
-                        roster_id = roster.insert_one(roster_doc).inserted_id
-                    except:
-                        pass
-
-
-                    #insert game
                     if(row[6] == "HOME"):
-                        home_team = team_id
-                        away_team = opponent_id
+                        home_team = school_1
+                        away_team = school_2
                     else:
-                        home_team = opponent_id
-                        away_team = team_id
-
-                    game_doc = {
-                        "date" : row[0],
-                        "home" : home_team,
-                        "away" : away_team,
-
-                    }
-                    try:
-                        game_id = game.insert_one(game_doc).inserted_id
-                    except:
-                        game_id = game.find_one({
-                            "$and" : [
-                                {"date": row[0]}, {"home": home_team}, {"away": away_team}
-                            ]
-                        }).get('_id')
+                        home_team = school_2
+                        away_team = school_1
 
                     if(row[7] == "MADE"):
                         make_bool = True
@@ -161,25 +73,110 @@ def importData():
                     else:
                         lama_bool = False
 
+                    if(row[5] in drafted_list):
+                        drafted_bool = True
+                    else:
+                        drafted_bool = False
 
+                    game_obj = game.find_one({
+                            "$and" : [
+                                {"date": row[0]}, {"home": home_team}, {"away": away_team}]})
+    
+                    if(game_obj == None):
+        
+                        game_doc = {
+                            "date" : row[0],
+                            "home" : home_team,
+                            "away" : away_team,
+                            "shots" : [ {
+                                "xloc" :row[3],
+                                "yloc" : row[4],
+                                "player_name" : row[5],
+                                "player_drafted": drafted_bool,
+                                "made" : make_bool,
+                                "type": row[8],
+                                "assist" : row[9],
+                                "points": points,
+                                "LAMA": lama_bool
+                             } ],
+                            }
 
-                    #insert shots
-                    shot_doc = {
+                        game_id = game.insert_one(game_doc).inserted_id
+
+                    else:
+                        game_id = game_obj.get("_id")
+                        shot_doc = {
                         "xloc" :row[3],
                         "yloc" : row[4],
-                        "game_id": game_id,
-                        "player_id" : player_id,
+                        "player_name" : row[5],
+                        "player_drafted": drafted_bool,
                         "made" : make_bool,
                         "type": row[8],
-                        "assist" : assist_id,
+                        "assist" : row[9],
                         "points": points,
                         "LAMA": lama_bool
-                    }
+                        }
 
-                    try:
-                        shot_id = shot.insert_one(shot_doc).inserted_id
-                    except:
-                        pass
+                        query = {"_id" : game_id}
+                        newvals = { "$push": { "shots": shot_doc } }
+                        
+                        game.update_one(query, newvals)
+
+         
+                    team_obj = team.find_one({"school" : school_1})
+
+                    print school_1
+
+                    if(team_obj == None):
+              
+                        if(school_1 == home_team):
+                            home_array = [game_id]
+                            away_array = []
+                        else:
+                            home_array = []
+                            away_array = [game_id]
+
+                        team_doc = {
+                            "school" : school_1,
+                            
+                           str(i) + "-" + str(i+1): {
+                            "tournament" : False,
+                            "home" : home_array,
+                            "away": away_array
+
+                           }
+                                   
+                        }
+                        team_id = team.insert_one(team_doc)
+                    
+                    else:
+
+                        season = str(i) + "-" + str(i+1)
+
+
+                        home_array = team_obj.get(season).get("home")
+                        away_array = team_obj.get(season).get("away")
+
+                        if(school_1 == home_team):
+                            home_array.append(game_id)
+                            home_or_away = "home"
+                        else:
+                           away_array.append(game_id)
+                           home_or_away = "away"
+
+                        new_season = {
+
+                            "tournament" : team_obj.get(season).get("tournament"),
+                            "home" : home_array,
+                            "away": away_array
+
+                        }
+                        str =  season+".$."+home_or_away
+                        query = {"_id": team_id}
+                        newvals = { "$push" : { str : game_id} }
+                        
+                        team.update_one(query, newvals)
+
     
     filename = "tournament-teams.csv"
     with open(filename) as csv_file:
@@ -189,24 +186,18 @@ def importData():
 
             if any (row):
                 
-                team_id = team.find_one({"school" : row[1]}).get('_id')
+                season_obj = team.find_one({"school" : row[1]}).get(row[0])
+                
+                new_season = {
+                                "tournament" : True,
+                                "home" : season_obj.get("home"),
+                                "away": season_obj.get("away"),
+                            }
 
-               
-                myquery = { "$and": [ { "season": row[0] },{"team_id": team_id} ] }
-                newvalues = {"$set": { "tournament": True }}
+                myquery = { "school" : row[1] }
+                newvals = {"$set": { row[0]: new_season }}
 
-                roster.update_one(myquery, newvalues)
-    '''
-    filename = "drafted.txt"
-    in_file = open(filename, 'r')
-    for line in in_file:
-        line = line.strip('\n')
-        query = {"name": line}
-        newvals = { "$set": { "drafted": True } }
-
-        player.update_one(query, newvals)
-
-    in_file.close()
+                team.update_one(myquery, newvals)
 
 
             
