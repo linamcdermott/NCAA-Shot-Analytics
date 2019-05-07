d14b9facf9639c5b62a55d592f0584daf1423fe9
Matt W <matt@matts-mbp.wireless.davidson.edu>
2019-05-07 04:31:03 -0400

    4:30 got queries working in correct div

--- homepage.php
+++ homepage.php
@@ -11,7 +11,7 @@
 
  <div class="description">
  <h1 style="position:relative;"><span>Ba$ket$</span></h1>
- <p>A shot analytics website.</p>
+ <p>A basketball shot analytics website.</p>
  <p> We scraped ESPN and created a database of NCAA Division I men's basketball shots from the past 
  six seasons. Our goal was to create a tool that could query the database and analyze the direction 
  of shot selection in college basketball. </p>
@@ -26,81 +26,68 @@
 
 
 
-<!-- STATS -->
 <br>
 <br>
-<!-- TODO - THERE HAS TO BE A BETTER WAY TO PAD RIGHT? -->
 
 
 
-<!-- </body> -->
 
 
- <div class="container">
-    <img src="/css/pictures/new_court.png">
 
-    <?php
-      require 'vendor/autoload.php';
-      $conn = new MongoDB\Client('mongodb://localhost');
-      $db = $conn->baskets;
-
-      $team = $db->team;
-      $shot = $db->shot;
-
-      $current_season = '2018-2019';
-      $current_team = 'DAVIDSON';
-      $current_lama = $current_assist = $current_home = $current_drafted = "both";
-
-      $all_teams = $team->find();
-      $teams = array();
-      foreach($all_teams as $t){
-        if (in_array($t["school"], $teams) == false){
-          
-          array_push($teams,$t["school"]);
-
-        }
-        
-      }
-      sort($teams);
-
-      
-      if(isset($_POST['submit'])){
-        // As output of $_POST['Season'] is an array we have to use foreach Loop to display individual value
-        foreach ($_POST['Season'] as $select)
-        {
-          $current_season = $select; // Displaying Selected Value
-        }
-
-        foreach ($_POST['teams'] as $select)
-        {
-          $current_team = $select; // Displaying Selected Value
-        }
-      }
-
-      // if(isset($_POST['submit'])){
-      //   // As output of $_POST['Color'] is an array we have to use foreach Loop to display individual value
-        
-      // }
-
-      if($current_team == 'ALL TEAMS'){
-        $team_selection = $team->find(array('season' => $current_season));
-      }
-      elseif ($current_team == "TOURNAMENT TEAMS"){
-        $team_selection = $team->find(array('tournament' => true, 'season' => $current_season));
-      }
-      elseif ($current_team == "NON-TOURNAMENT TEAMS"){
-        $team_selection = $team->find(array('tournament' => false,'season' => $current_season));
-      }
-      else{
-        $team_selection = $team->find(array('school' => $current_team,'season' => $current_season));
-      }
-?>
-
-
- <div class="selector">
+<div class="selector">
 <p style="font-weight:bold">Filter shot database:</p>
 
 <?php 
+
+require 'vendor/autoload.php';
+$conn = new MongoDB\Client('mongodb://localhost');
+$db = $conn->baskets;
+
+$team = $db->team;
+$shot = $db->shot;
+
+$current_season = '2018-2019';
+$current_team = 'DAVIDSON';
+$current_lama = $current_assist = $current_home = $current_drafted = "both";
+
+$all_teams = $team->find();
+$teams = array();
+foreach($all_teams as $t){
+  if (in_array($t["school"], $teams) == false){
+    
+    array_push($teams,$t["school"]);
+
+  }
+  
+}
+sort($teams);
+
+
+if(isset($_POST['submit'])){
+  // As output of $_POST['Season'] is an array we have to use foreach Loop to display individual value
+  foreach ($_POST['Season'] as $select)
+  {
+    $current_season = $select; // Displaying Selected Value
+  }
+
+  foreach ($_POST['teams'] as $select)
+  {
+    $current_team = $select; // Displaying Selected Value
+  }
+}
+
+if($current_team == 'ALL TEAMS'){
+  $team_selection = $team->find(array('season' => $current_season));
+}
+elseif ($current_team == "TOURNAMENT TEAMS"){
+  $team_selection = $team->find(array('tournament' => true, 'season' => $current_season));
+}
+elseif ($current_team == "NON-TOURNAMENT TEAMS"){
+  $team_selection = $team->find(array('tournament' => false,'season' => $current_season));
+}
+else{
+  $team_selection = $team->find(array('school' => $current_team,'season' => $current_season));
+}
 
 $makes = 0;
 $misses = 0;
@@ -123,13 +110,8 @@ foreach($all_years as $yr){
   }
  
 }
-  
-  // <option value='2014-2015'>2014-2015</option>
-  // <option value='2015-2016'>2015-2016</option>
-  // <option value='2016-2017'>2016-2017</option>
-  // <option value='2017-2018'>2017-2018</option>
-  // <option value='2018-2019'>2018-2019</option>
-  echo "</select> <br><br>";
+
+echo "</select> <br><br>";
 
 
 
@@ -268,106 +250,90 @@ function test_input($data) {
       $current_drafted = array('$ne' => null);
     }
 
-
-    $shot_makes = $shot ->find(['team_id' => $team['_id'],  'made' => true, 'LAMA' => $query_lama, 'assist' => $query_assist, 'player_drafted' => $query_drafted, 'home' => $query_home]);
-    //$shot_makes = $shot ->find(['team_id' => $team['_id'],  'made' => true, 'LAMA' => $query_lama]);
-    $shot_misses = $shot ->find(['team_id' => $team['_id'],  'made' => false, 'LAMA' => $query_lama, 'assist' => $query_assist, 'player_drafted' => $query_drafted, 'home' => $query_home]);
-    //$make_count = count($shot_makes);
-    //$shot_misses = $shot ->find(['team_id' => $team['_id'],  'made' => false, 'LAMA' => $query_lama]);
-    // echo "<p> $make_count </p>";
-    
-    foreach($shot_makes as $row){
-      $right = $row['yloc'];
-      $left = $row['xloc'] * 1.8;
-      $makes++;
-      $points += $row['points'];
-      if ($row['assist'] != 'n/a'){
-        $assists += 1;
-      }
-      if($row['LAMA'] == true){
-        $LAMA += 1;
-      }
-      echo "<span class = \"dot_make\" style= \"position:absolute;right:$right%;bottom:$left%;\"> </span>";
-    }
-    $count2 = 0;
-    foreach($shot_misses as $row){
-      $right = $row['yloc'];
-      $left = $row['xloc'] * 1.8;
-      $misses++;
-      if($row['LAMA'] == true){
-        $LAMA += 1;
-      }
-      echo "<span class = \"dot_miss\" style= \"position:absolute;right:$right%;bottom:$left%;\"> </span>";
-    }
-  }
-
-   
-  $total = $makes + $misses;
-  if($total <= 0){
-    $FG = 0.00;
-    $PPS = 0.00;
-    $AST = 0.00;
-    $lp = 0.00;
-  }
-  else{
-    $FG = round($makes / ($total) * 100,1);
-    $PPS = round($points / ($total),3);
-    $AST = round($assists / $makes * 100,1);
-    $lp = round($LAMA / ($total) * 100,1);
-  }
-
-
-echo"
-  <p style=\"position:absolute;top:100%;\"> Shots Plotted: $total</p>   
-  </div>
-  <table id=\"statsTable\">
-  <tr>
-  <td class=\"tooltip\">$FG%
-  <span class='tooltiptext'>Field Goal Percentage</span></td>
-  <td class=\"tooltip\">$PPS
-  <span class='tooltiptext'>Average points per shot</span></td>
-  <td class=\"tooltip\">$AST%
-  <span class='tooltiptext'>Percentage of assisted made shotes</span></td>
-  <td class=\"tooltip\">$lp%
-  <span class='tooltiptext'>Percentage of shots that occur either behind the threepoint line or right at the basket (layup, dunk, etc.)</span></td>
-  </tr>
-  <tr class=\"statsTableBottomRow\">
-  <td>FG%</td>
-  <td>PPS</td>
-  <td>AST%</td>
-  <td>LAMA%</td>
-  </tr> 
-";
-
-
 ?> 
 
 
+</div>
 
 
-<!-- BUTTONS -->
-<!-- <ol>
-  <button>Home Games</button>
-  <button>Away Games</button>
-  <br>
-  <br>
-  <button>Assisted Shots</button>
-  <button>Unassisted Shots</button>
-  <br>
-  <br>
-  <button>LAMA Shots</button>
-  <button>Non-Lama Shots</button>
-  <br>
-  <br>
-  <button>Only Shots Taken by Future NBA Draft Picks</button>
-  <br>
-  <br>
-  <br>
-  <button class=plot>PLOT</button> 
-  <button class=reset>RESET</button>
-</ol> -->
 
- </div>
+
+<div class="container">
+<img src="/css/pictures/new_court.png">
+
+<?php
+      
+$shot_makes = $shot ->find(['team_id' => $team['_id'],  'made' => true, 'LAMA' => $query_lama, 'assist' => $query_assist, 'player_drafted' => $query_drafted, 'home' => $query_home]);
+//$shot_makes = $shot ->find(['team_id' => $team['_id'],  'made' => true, 'LAMA' => $query_lama]);
+$shot_misses = $shot ->find(['team_id' => $team['_id'],  'made' => false, 'LAMA' => $query_lama, 'assist' => $query_assist, 'player_drafted' => $query_drafted, 'home' => $query_home]);
+//$make_count = count($shot_makes);
+//$shot_misses = $shot ->find(['team_id' => $team['_id'],  'made' => false, 'LAMA' => $query_lama]);
+// echo "<p> $make_count </p>";
+
+foreach($shot_makes as $row){
+  $right = $row['yloc'];
+  $left = $row['xloc'] * 1.8;
+  $makes++;
+  $points += $row['points'];
+  if ($row['assist'] != 'n/a'){
+    $assists += 1;
+  }
+  if($row['LAMA'] == true){
+    $LAMA += 1;
+  }
+  echo "<span class = \"dot_make\" style= \"position:absolute;right:$right%;bottom:$left%;\"> </span>";
+}
+$count2 = 0;
+foreach($shot_misses as $row){
+  $right = $row['yloc'];
+  $left = $row['xloc'] * 1.8;
+  $misses++;
+  if($row['LAMA'] == true){
+    $LAMA += 1;
+  }
+  echo "<span class = \"dot_miss\" style= \"position:absolute;right:$right%;bottom:$left%;\"> </span>";
+}
+}
+
+
+$total = $makes + $misses;
+if($total <= 0){
+$FG = 0.00;
+$PPS = 0.00;
+$AST = 0.00;
+$lp = 0.00;
+}
+else{
+$FG = round($makes / ($total) * 100,1);
+$PPS = round($points / ($total),3);
+$AST = round($assists / $makes * 100,1);
+$lp = round($LAMA / ($total) * 100,1);
+}
+
+
+echo"
+<p style=\"position:absolute;top:100%;\"> Shots Plotted: $total</p>   
+</div>
+<table id=\"statsTable\">
+<tr>
+<td class=\"tooltip\">$FG%
+<span class='tooltiptext'>Field Goal Percentage</span></td>
+<td class=\"tooltip\">$PPS
+<span class='tooltiptext'>Average points per shot</span></td>
+<td class=\"tooltip\">$AST%
+<span class='tooltiptext'>Percentage of assisted made shotes</span></td>
+<td class=\"tooltip\">$lp%
+<span class='tooltiptext'>Percentage of shots that occur either behind the threepoint line or right at the basket (layup, dunk, etc.)</span></td>
+</tr>
+<tr class=\"statsTableBottomRow\">
+<td>FG%</td>
+<td>PPS</td>
+<td>AST%</td>
+<td>LAMA%</td>
+</tr> 
+";
+      
+?>
 
 
 
